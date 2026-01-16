2f519034e05074a878007d48c0ade348f9cbf582
allegramaoloni <allegra.maoloni@edu.unifi.it>
2026-01-16 15:09:52 +0100

    Creata classe per avviare tutti i test

---
+++ RunallTest.cpp
@@ -0,0 +1,9 @@
+//
+// Created by Allegra Maoloni on 16/01/26.
+//
+#include <gtest/gtest.h>
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
