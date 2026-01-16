dfdedcddaa7d9e0ad8297ef4ee7754ae1a9bf502
allegramaoloni <allegra.maoloni@edu.unifi.it>
2026-01-16 14:56:33 +0100

    Creazione classe TaskListException per gestire eccezioni in modo più specifico

---
+++ TaskListException.h
@@ -0,0 +1,14 @@
+//
+// Created by Allegra Maoloni on 16/01/26.
+//
+
+#ifndef TODOLIST_TASKLISTEXCEPTION_H
+#define TODOLIST_TASKLISTEXCEPTION_H
+
+#include <stdexcept>
+#include <string>
+class TaskListException : public std::runtime_error{
+public:
+    explicit TaskListException(const std::string& what) : std::runtime_error(what) {}
+};
+#endif //TODOLIST_TASKLISTEXCEPTION_H
