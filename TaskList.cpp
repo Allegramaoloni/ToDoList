aea45d85c961f83d4bac1366d8b7ff1165b900fb
allegramaoloni <allegra.maoloni@edu.unifi.it>
2026-02-12 14:07:04 +0100

    Implementazione del metodo per leggere da disco

--- TaskList.cpp
+++ TaskList.cpp
@@ -44,7 +44,7 @@ std::string TaskList::getTitolo(){
 #include <fstream>
 
 void TaskList::saveToFile(const std::string& filename) {
-    std::ofstream file(filename);
+    std::ofstream file(filename);//ofstream serve epr scrivere dati su un file, è tipo cout ma invece di stampare su schermo stampa su file
 
     if (!file) {
         std::cerr << "Errore apertura file in scrittura\n";
@@ -55,6 +55,38 @@ void TaskList::saveToFile(const std::string& filename) {
         file << task.getTitolo() << ";"
              << task.getIsCompleted() << ";"
              << task.getPriority() << "\n";
+    }
+
+    file.close();
+}
+#include <sstream>
+
+void TaskList::loadFromFile(const std::string& filename) {
+    std::ifstream file(filename);//serve per leggere dati
+
+    if (!file) {
+        std::cerr << "Errore apertura file in lettura\n";
+        return;
+    }
+
+    listaTask.clear();
+
+    std::string line;
+    while (std::getline(file, line)) {
+        std::stringstream ss(line);
+
+        std::string titolo;
+        std::string completedStr;
+        std::string priorityStr;
+
+        std::getline(ss, titolo, ';');
+        std::getline(ss, completedStr, ';');
+        std::getline(ss, priorityStr, ';');
+
+        bool completed = std::stoi(completedStr);//serve a convertire una stringa in intero
+        int priority = std::stoi(priorityStr);
+
+        listaTask.push_back(Task(titolo, completed, priority));
     }
 
     file.close();
